201e87e51865feaa97da71b640e26444a8549ecd
LaudemPax <aman.tifli@gmail.com>
2022-07-14 17:47:18 +0200

    created Java project

---
+++ vertifeungen-projekte/java-projects/desktop/src/de/augsburg/hs/methoden/ki/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package de.augsburg.hs.methoden.ki;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import de.augsburg.hs.methoden.ki.MainGame;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("methoden-der-KI");
+		new Lwjgl3Application(new MainGame(), config);
+	}
+}
